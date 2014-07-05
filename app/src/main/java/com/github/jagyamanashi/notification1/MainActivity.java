84f130a280a3c69ac16eec0c87540c8ef4d7bd1e
Yuuki Shimizu <yuuki@maxio.jp>
2014-07-05 17:29:52 +0900

    ボタンを配置

--- app/src/main/java/com/github/jagyamanashi/notification1/MainActivity.java
+++ app/src/main/java/com/github/jagyamanashi/notification1/MainActivity.java
@@ -2,18 +2,30 @@ package com.github.jagyamanashi.notification1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "Button clicked.");
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
